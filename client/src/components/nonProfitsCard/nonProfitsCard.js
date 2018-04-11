b2446ec1450825b7726abe166e9473644e7337a6
michellele994 <michellele@email.arizona.edu>
2018-04-11 14:22:14 -0700

    Switched 'foodpostby' and title in cards. Made the links white so we can seethem

--- client/src/components/nonProfitsCard/nonProfitsCard.js
+++ client/src/components/nonProfitsCard/nonProfitsCard.js
@@ -4,7 +4,7 @@ import React from "react";
 const NonProfitsCard = (props) => {
   return (
     <div className="card text-black border-primary my-3" >
-        <div className="card-header bg-primary"><a href={"/NonProfitProfile/"+props.nonProfitId}><h5>{props.nonProfitName}</h5></a></div>
+        <div className="card-header bg-primary"><a className="text-white" href={"/NonProfitProfile/"+props.nonProfitId}><h5>{props.nonProfitName}</h5></a></div>
         <div className="card-body">
             <h5 onClick={()=>props.cardClicked(props.nonProfitAddress)} className="card-title">Address: {props.nonProfitAddress}</h5>
             <div className ="row">
