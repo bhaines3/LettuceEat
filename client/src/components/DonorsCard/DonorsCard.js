9906c5af6fb68425c2c372cc754679c585f64b7a
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-04-11 14:51:36 -0700

    email link working change color for cards to white

--- client/src/components/DonorsCard/DonorsCard.js
+++ client/src/components/DonorsCard/DonorsCard.js
@@ -4,7 +4,7 @@ import React from "react";
 const DonorsCard = (props) => {
   return (
     <div className="card text-black border-primary my-3">
-        <div className="card-header bg-primary"><a href={"/DonorProfile/"+props.donorId}><h5>{props.donorName}</h5></a></div>
+        <div className="card-header bg-primary"><a style={{color:"white"}}href={"/DonorProfile/"+props.donorId}><h5>{props.donorName}</h5></a></div>
         <div  className="card-body">
             <h5 onClick={()=>props.cardClicked(props.donorAddress)} className="card-title">Address: {props.donorAddress}</h5>
             <div className ="row">
