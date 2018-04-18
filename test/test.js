2ddffb521d02b93d8c45cda4ee9a8ce985f79a58
Brandon Haines <haines.brandon.91@gmail.com>
2018-04-17 19:53:41 -0700

    updated donor tests and background

--- test/test.js
+++ test/test.js
@@ -72,6 +72,85 @@ describe("The inital get requests", () => {
     });
 });
 
-//BRANDON YOUR CODE GOES HERE
+//DONOR TEST
+describe("The creation of a donor", () => {
+    let server;
+    beforeEach(() => {
+        delete require.cache[require.resolve('../server')];
+        return require("../server.js").then(s => server = s);
+    });
+    before(() => {
+        usersController.clearAllUsers();
+        donorController.clearAllDonors();
+        nonProfitController.clearAllNonProfits();
+    });
+    // afterEach(done => server.close(done));
+    it("should intially return an empty users database.", done => {
+        chai.request(server)
+            .get("/api/users/")
+            .end((err, res) => {
+                expect(200)
+                res.body.should.be.a('array')
+                expect(res.body).to.have.lengthOf(0);
+                done();
+            })
+    });
+    it("should create a user who is a donor", done => {
+        let newUserInfo = {
+            email: "donor@gmail.com",
+            name: "DonorTest",
+            location: "100 S. Stone Ave Tucson, AZ 85701",
+            isDonor: "true",
+            phonenumber: "5205555555",
+            password: "testpassword"
+        }
+        chai.request(server)
+            .post("/api/signup/")
+            .send(newUserInfo)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.success.should.be.true;
+                expect(res.body.msg).to.eql("Successful created new user.");
+                done();
+            });
+    });
+    it("should login the newly created donor", done => {
+        let loginUser = {
+            email: "donor@gmail.com",
+            password: "testpassword"
+        }
+        chai.request(server)
+            .post("/api/login/")
+            .send(loginUser)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.success.should.be.true;
+                done();
+            });
+    });
+    it("should GET all of the existing users and return a length of 1, accomodating new user", done => {
+        chai.request(server)
+            .get("/api/users/")
+            .end((err, res) => {
+                expect(200)
+                res.body.should.be.a('array')
+                expect(res.body).to.have.lengthOf(1);
+                done();
+            })
+    });
+    it("should GET all of the existing donors and return a length of 1, accomodating new donor", done => {
+        chai.request(server)
+            .get("/api/donors/")
+            .end((err, res) => {
+                donorId = res.body.id;
+                expect(200)
+                res.body.should.be.a('array')
+                expect(res.body).to.have.lengthOf(1);
+                done();
+            })
+    });
+});
 
 //ERNESTO YOUR CODE GOES HERE
